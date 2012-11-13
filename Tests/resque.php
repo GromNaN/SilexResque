d21d0c8e78ba420d28704b49245c7decd70d7d44
Jérôme Tamarelle <jerome@tamarelle.net>
2012-11-13 11:54:33 +0100

    Inject the application into the command

--- Tests/resque.php
+++ Tests/resque.php
@@ -2,8 +2,10 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+$app = new Silex\Application();
+
 $console = new Symfony\Component\Console\Application();
 
-$console->add(new Grom\SilexResque\Command\ResqueWorkerCommand());
+$console->add(new Grom\SilexResque\Command\ResqueWorkerCommand($app));
 
 $console->run();
